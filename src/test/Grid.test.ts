cc6aeba8c46cc9154d049cda483d66b91e461071
Kuba429 <jakubzawadzki429@gmail.com>
2022-07-11 18:23:07 +0200

    more testing

--- src/test/Grid.test.ts
+++ src/test/Grid.test.ts
@@ -1,10 +1,17 @@
 import { JSDOM } from "jsdom";
+import { CellState } from "../Cell";
 import Grid from "../Grid";
 
 const dom = await JSDOM.fromFile("index.html");
 global.document = dom.window.document;
 
 const grid = new Grid(document.querySelector("#grid")!);
+
+beforeEach(() => {
+	grid.settingsElement.reset();
+	grid.settingsElement.dispatchEvent(new Event("submit"));
+});
+
 describe("Grid", () => {
 	it("First cell can't be a bomb", () => {
 		let didLose = false;
@@ -21,6 +28,24 @@ describe("Grid", () => {
 			grid.settingsElement.dispatchEvent(new Event("submit"));
 		}
 		expect(didLose).toBe(false);
+		grid.settingsElement.dispatchEvent(new Event("submit"));
+	});
+	it("Flag a cell on right click", () => {
+		let isFailed = false;
+		const cells = grid.grid.flat();
+		const count = Math.min(Math.floor(cells.length / 4), 25);
+		for (let i = 0; i < count; i++) {
+			const randomIndex = Math.round(Math.random() * (cells.length - 1));
+			cells[randomIndex].element.dispatchEvent(new Event("contextmenu"));
+			cells[randomIndex].element.dispatchEvent(new Event("pointerup"));
+			if (cells[randomIndex].state !== CellState.FLAG) {
+				isFailed = true;
+				break;
+			}
+			cells.splice(randomIndex, 1);
+		}
+		expect(isFailed!).toBe(false);
+		expect(grid.flaggedCount).toBe(count);
 	});
 });
 
